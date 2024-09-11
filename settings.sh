29f3ba89ff39a5ef7af760eda5d82e34d18c76b8
mjcarbonell <maximcarbonell@gmail.com>
2024-09-11 09:25:37 -0700

    TRUE

--- settings.sh
+++ settings.sh
@@ -3,6 +3,6 @@ export INSTALL_JAR="forge-1.16.5-36.2.35-installer.jar"
 export SERVER_JAR="forge-1.16.5-36.2.35.jar"
 
 # You can edit these values if you wish.
-export MIN_RAM="1024M"
-export MAX_RAM="4096M"
+export MIN_RAM="4G"
+export MAX_RAM="4G"
 export JAVA_PARAMETERS="-XX:+UseG1GC -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -Dfml.readTimeout=180"
