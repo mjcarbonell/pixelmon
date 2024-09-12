4cec499b81db02fdebdd7f30c5c79adf78ca6cbf
mjcarbonell <maximcarbonell@gmail.com>
2024-09-12 00:03:59 -0700

    trying

--- backup.sh
+++ backup.sh
@@ -1,9 +1,2 @@
-#!/bin/sh
 
-# Email settings
-RECIPIENT="maximcarbonell@gmail.com"
-SUBJECT="Just Saying Hey!"
-MESSAGE="Hey!"
-
-# Send the email
-echo "$MESSAGE" | mail -s "$SUBJECT" $RECIPIENT
+echo save-all > input.txt
